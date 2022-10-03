f8ef9f55ec675b291d1e507510f7b3fc0ae2d486
alireza_mo <alirezamomenpour01@gmail.com>
2022-10-03 20:49:57 +0330

    add app name to logo view

--- app/src/main/java/com/digimoplus/moboplayer/presentation/componnets/LogoView.kt
+++ app/src/main/java/com/digimoplus/moboplayer/presentation/componnets/LogoView.kt
@@ -2,16 +2,18 @@ package com.digimoplus.moboplayer.presentation.componnets
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.digimoplus.moboplayer.R
+import com.digimoplus.moboplayer.presentation.theme.LightGray
 
 @Composable
 fun LogoView() {
@@ -21,10 +23,19 @@ fun LogoView() {
             .background(color = Color(0xFF000000)),
         contentAlignment = Alignment.Center,
     ) {
-        Image(
-            modifier = Modifier.size(300.dp),
-            painter = painterResource(R.drawable.logo),
-            contentDescription = null
-        )
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                modifier = Modifier.size(300.dp),
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null
+            )
+            Spacer(Modifier.padding(top = 20.dp))
+            Text(
+                text = "Mobo Player",
+                color = LightGray,
+                style = MaterialTheme.typography.body1,
+                fontSize = 30.sp,
+            )
+        }
     }
 }
